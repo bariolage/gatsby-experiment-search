17f7d54fe5c5e3f6ca06e7272eaf9c8a67e74d03
Ju <40365206+bariolage@users.noreply.github.com>
2020-02-21 10:50:45 +0100

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,48 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `gatsby search`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `assets`,
+        path: `${__dirname}/assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-csv`,
+      options: {
+        delimiter: ";",
+      },
+    },
+    {
+      resolve: "gatsby-plugin-local-search",
+      options: {
+        name: "pages",
+        engine: "flexsearch",
+        engineOptions: {
+          tokenize: "forward",
+        },
+        query: `
+          {
+            allEwokBaseCsv {
+              nodes {
+                name
+                id
+                description
+              }
+            }
+          }
+        `,
+        ref: "id",
+        normalizer: ({ data }) =>
+          data.allEwokBaseCsv.nodes.map(node => ({
+            id: node.id,
+            title: node.name,
+            body: node.description,
+          })),
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,37 @@
-import React from "react"
+import React, { useState } from "react"
+import { useFlexSearch } from "react-use-flexsearch"
+import { Formik, Form, Field } from "formik"
 
-export default () => <div>Hello world!</div>
+export default ({ data: { search } }) => {
+  const [query, setQuery] = useState(null)
+  const results = useFlexSearch(query, search.index, JSON.parse(search.store))
+  return (
+    <div>
+      <Formik initialValues={{ query: "" }}>
+        <Form
+          type="text"
+          onChange={e => {
+            setQuery(e.target.value)
+          }}
+          name="name"
+        >
+          <Field name="query" placeholder="search an ewok" />
+        </Form>
+      </Formik>
+      <ul>
+        {results.map(result => (
+          <li key={result.id}>{result.title}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export const pageQuery = graphql`
+  query {
+    search: localSearchPages {
+      store
+      index
+    }
+  }
+`
